da30f4c796b27cc80bb90c5701ffb8240ac59d33
Timmy Lin <tlin@circle.com>
2021-05-28 23:28:11 -0400

    Add 3DS

--- plugins/paymentsApi.ts
+++ plugins/paymentsApi.ts
@@ -22,7 +22,7 @@ export default ({ store }: any, inject: any) => {
   instance.interceptors.request.use(
     function (config) {
       store.commit('CLEAR_REQUEST_DATA')
-      store.commit('SET_REQUEST_URL', `${config.baseURL}${config.url}`)
+      store.commit('SET_REQUEST_URL', `${config.url}`)
       store.commit('SET_REQUEST_PAYLOAD', config.data)
 
       if (store.state.bearerToken) {
